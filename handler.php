d981f54929046308f1b53c0320ce86f7e8232f77
AidenVu1 <100857445+AidenVu1@users.noreply.github.com>
2023-03-28 10:06:16 -0400

    Update handler.php

--- handler.php
+++ handler.php
@@ -9,30 +9,27 @@
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
         <link rel="stylesheet" href="index.css">
         <title> php </title>
+</head>
         <body>
-        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header class="mdl-layout__header">
+                  <div class="mdl-layout__header-row">
+                    <span class="mdl-layout-title">area and circumference of a circle</span>
+                  </div>
+                </header>
+                <main class="mdl-layout__content" style="margin-left:75px;">
 
-<header class="mdl-layout__header">
-  <div class="mdl-layout__header-row">
-    <span class="mdl-layout-title">area and circumference of a circle</span>
-  </div>
-</header>
-
-<main class="mdl-layout__content" style="margin-left:75px;">
-
-  <?php
-  $_POST['diameter1']=$_POST['diameter']/2
-  ?>
-  <?php
-    echo "<h3>the circumference of a circle with a diameter of ".$_POST['diameter']." is ".$_POST['diameter1']*2*pi()."<h3>";
-  ?>
-  <?php
-    echo "<h3>the area of a circle with a diameter of ".$_POST['diameter']." is ".$_POST['diameter1']**2*pi()."<h3>";
-  ?>
+<?php
+$_POST['diameter1']=$_POST['diameter']/2
+?>
+<?php
+  echo "<h3>the circumference of a circle with a diameter of ".$_POST['diameter']." is ".$_POST['diameter1']*2*pi()."<h3>";
+?>
+<?php
+  echo "<h3>the area of a circle with a diameter of ".$_POST['diameter']." is ".$_POST['diameter1']**2*pi()."<h3>";
+?>
 </main>
 
 </div>
-        
-        </body>
-    </head>
+</body>
 </html>
+
